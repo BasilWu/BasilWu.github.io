fd2d76367cf8cd702d788c2adcf708eedce4d772
BasilWu <mosstackstudio@gmail.com>
2026-08-17 07:28:08 +0000

    style: 加入網站流量分析追蹤碼

--- build-blog.js
+++ build-blog.js
@@ -437,6 +437,14 @@ function generateArticleHTML({ title, slug, date, category, description, readTim
       margin-bottom: var(--space-3);
     }
   </style>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-GYTGRMGCSH"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-GYTGRMGCSH');
+    </script>
 </head>
 <body>
 <div class="article-progress" aria-hidden="true"></div>
